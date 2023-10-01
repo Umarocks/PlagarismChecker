072c13cdc8cdda31011db12b7a28b359e15d2fbd
Umarocks <umarocks@hotmail.com>
2023-09-30 20:43:39 -0500

    added the function to parse and compute string as per logic required splitting it based on '.' over to you now

--- webCrawler.py
+++ webCrawler.py
@@ -1,7 +1,10 @@
 from googlesearch import search
 import requests
 from bs4 import BeautifulSoup
-
+import gensim
+from gensim import corpora
+from gensim import similarities
+import nltk
 # Define the search query
 query = "The company said the new phones' titanium frame and aluminum substructure aren't contributing to the issue, and that they dissipate heat better than the stainless steel used in prior Pro models"
 
@@ -31,8 +34,9 @@ def parse_url(url):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching {url}: {str(e)}")
         return None
-
+reference_documents=[]
 # Parse and print the data from each URL
+sentences = []
 for i, url in enumerate(urls, start=1):
     print(f"Processing URL {i}: {url}")
     soup = parse_url(url)
@@ -40,10 +44,22 @@ for i, url in enumerate(urls, start=1):
         headings_and_paragraphs = [tag.text.strip() for tag in soup.find_all([ 'p', 'a'])]
         # Extract text within a tags
         filtered_headings_and_paragraphs = [text for text in headings_and_paragraphs if len([word for word in text.split() if word.strip()]) > 4]
-        output_string = ' '.join(filtered_headings_and_paragraphs)
-        # Print the extracted text
-        print("Headings:", output_string)
-     
-        
+        for i in filtered_headings_and_paragraphs:
+            if '.' in i:
+                # Split the string based on periods
+                substrings = i.split('.')
+                
+                # Remove empty strings (if any)
+                substrings = [substring.strip() for substring in substrings if substring.strip()]
+                
+                # Extend the final list with the split substrings
+                sentences.extend(substrings)
+            else:
+                # No period found, append the whole string
+                sentences.append(i)
+        # Print the array of sentences
+        for sentence in sentences:
+            print(sentence)
+  
     print("\n" + "-" * 40 + "\n")  # Separator between URLs
 
