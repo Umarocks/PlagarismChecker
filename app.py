df41b080c7008b4475221588de6266a57019d7c3
Umarocks <umarocks@hotmail.com>
2023-10-01 10:06:37 -0500

    stuck with divide by 0 error

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ import gensim
 from gensim import corpora
 from gensim import similarities
 import nltk
-from flask import Flask, render_template, jsonify, request  # Import 'request'
+from flask import Flask, render_template, jsonify, request  
+import string
 
 app = Flask(__name__)
 
@@ -24,6 +25,17 @@ reference_documents = []
 sentences = []
 score_tracker = 0
 score = [0]
+
+
+
+# Function to calculate Jaccard similarity between two sets
+def jaccard_similarity(set1, set2):
+    intersection = set1.intersection(set2)
+    union = set1.union(set2)
+    if(len(intersection)==0 or len(union)==0):
+        return 
+    return (len(intersection) / len(union))
+
 # Function to tokenize and generate n-grams from a given text
 def generate_ngrams(text, n):
     tokens = nltk.word_tokenize(text)
@@ -33,20 +45,13 @@ def generate_ngrams(text, n):
         ngrams.append(ngram)
     return ngrams
 
-# Function to calculate Jaccard similarity between two sets
-def jaccard_similarity(set1, set2):
-    intersection = set1.intersection(set2)
-    union = set1.union(set2)
-    if(intersection==0 or union==0):
-        return 
-    
-    return (len(intersection) / len(union))
-
 # Function to check for plagiarism using n-grams analysis
-def detect_plagiarism(source_text, suspicious_text, n=3, threshold=0.5):
+def detect_plagiarism(source_text, suspicious_text, n, threshold):
     source_ngrams = set(generate_ngrams(source_text, n))
     suspicious_ngrams = set(generate_ngrams(suspicious_text, n))
     similarity = jaccard_similarity(source_ngrams, suspicious_ngrams)
+    if(similarity!=0):
+        print(score[score_tracker]," SCORE PER SENTENCE ",similarity)
     score[score_tracker]=max(score[score_tracker],similarity)
 
 def compute_plagarism():
@@ -55,27 +60,12 @@ def compute_plagarism():
     source_text = sus_sentence
     for sentence in sentences:
         suspicious_text = sentence
-        is_plagiarism = detect_plagiarism(source_text, suspicious_text, n=3, threshold=0.5)    
+        is_plagiarism = detect_plagiarism(source_text, suspicious_text, 3, threshold=0.5)    
     score_tracker += 1  
     score.append(0)
 
-   
 
-def get_url():
-    # Perform the Google search and get search results
-    for sus_sentence in query:
-        search_results = list(search(sus_sentence, num_results=4))  # Adjust the number as needed
-        # Extract the top 5 URLs from the search results
-        top_links = search_results[:2]      
-        # put linkes in urls array
-        for i, link in enumerate(top_links, start=1):
-            urls.append(link)
-        # for i in urls:
-        #     print("URL:", i)
-        string_cleaning()
-        urls.clear()
 
-# Function to fetch and parse a URL
 def parse_url(url):
     try:
         response = requests.get(url)
@@ -88,61 +78,7 @@ def parse_url(url):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching {url}: {str(e)}")
         return None
-    
-# Parse and print the data from each URL
 
-def string_cleaning():
-    for i, url in enumerate(urls, start=1):
-        # print(f"Processing URL {i}: {url}")
-        soup = parse_url(url)
-        if soup:
-            headings_and_paragraphs = [tag.text.strip() for tag in soup.find_all([ 'p', 'a'])]
-            # Extract text within a tags
-            filtered_headings_and_paragraphs = [text for text in headings_and_paragraphs if len([word for word in text.split() if word.strip()]) > 4]
-            # print(filtered_headings_and_paragraphs)
-            # for i in filtered_headings_and_paragraphs:
-            #     if '.' in i:
-            #         # Split the string based on periods
-            #         substrings = i.split('.')
-                    
-            #         # Remove empty strings (if any)
-            #         substrings = [substring.strip() for substring in substrings if substring.strip()]
-                    
-            #         # Extend the final list with the split substrings
-            #         sentences.extend(substrings)
-            #     else:
-            #         # No period found, append the whole string
-            #         sentences.append(i)
-            # # Print the array of sentences
-            # # for sentence in sentences:
-            # #     print(sentence)
-            for i in filtered_headings_and_paragraphs:
-                if '.' in i:
-                    # Split the string based on periods
-                    substrings = i.split('.')
-                    
-                    # Remove empty strings (if any)
-                    substrings = [substring.strip() for substring in substrings if substring.strip()]
-                    
-                    # Remove newline characters and citations
-                    substrings = [substring for substring in substrings if not (substring.startswith('[[') and substring.endswith(']'))]
-                    
-                    # Extend the final list with the split substrings
-                    sentences.extend(substrings)
-                else:
-                    # Remove newline characters and citations
-                    i = i.strip()
-                    i = i.strip('[[]]')
-                    
-                    # No period found, append the whole string
-                    sentences.append(i)
-            global cleaned_textss 
-            cleaned_textss= clean_text(sentences)
-            print("SENTENCES CLEANED: ",cleaned_textss )
-        # print("\n" + "-" * 40 + "\n")  # Separator between URLs
-    compute_plagarism()
-    cleaned_textss.clear()
-import string
 def clean_text(input_texts):
     # Create a translation table to remove punctuation marks
     translator = str.maketrans('', '', string.punctuation)  
@@ -154,16 +90,45 @@ def clean_text(input_texts):
         cleaned_texts.append(cleaned_text)
     return cleaned_texts
 
+def string_cleaning():
+    for i, url in enumerate(urls, start=1):
+        soup = parse_url(url)
+        if soup:
+            headings_and_paragraphs = [tag.text.strip() for tag in soup.find_all([ 'p', 'a'])]
+            # Extract text within a tags
+            filtered_headings_and_paragraphs = [text for text in headings_and_paragraphs if len([word for word in text.split() if word.strip()]) > 4]
+            for i in filtered_headings_and_paragraphs:
+                if '.' in i:
+                    # Split the string based on periods
+                    substrings = i.split('.')  
+                    # Remove empty strings (if any)
+                    substrings = [substring.strip() for substring in substrings if substring.strip()]
+                    # Remove newline characters and citations
+                    substrings = [substring for substring in substrings if not (substring.startswith('[[') and substring.endswith(']'))]
+                    # Extend the final list with the split substrings
+                    sentences.extend(substrings)
+                else:
+                    # Remove newline characters and citations
+                    i = i.strip()
+                    i = i.strip('[[]]')     
+                    # No period found, append the whole string
+                    sentences.append(i)
+            global cleaned_textss 
+            cleaned_textss= clean_text(sentences)          
+    compute_plagarism()
+    cleaned_textss.clear()
 
-def filtering(input_string , query):
-    # Split the input string into an array of strings using periods as the delimiter
-    sentences = input_string.split(".")
-    # Remove leading/trailing spaces and empty strings
-    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
-    # Print the resulting array of sentences
-    temp_sentences=clean_text(sentences)
-    # print(temp_sentences)
-    query.extend(temp_sentences)
+def get_url():
+    # Perform the Google search and get search results
+    for sus_sentence in query:
+        search_results = list(search(sus_sentence, num_results=4))  # Adjust the number as needed
+        # Extract the top 5 URLs from the search results
+        top_links = search_results[:2]      
+        # put linkes in urls array
+        for i, link in enumerate(top_links, start=1):
+            urls.append(link)
+        string_cleaning()
+        urls.clear()
     
 
 def average_without_last_element(arr):
@@ -176,8 +141,16 @@ def average_without_last_element(arr):
     
     return average
 
+def filtering(input_string , query):
+    # Split the input string into an array of strings using periods as the delimiter
+    sentences = input_string.split(".")
+    # Remove leading/trailing spaces and empty strings
+    sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+    temp_sentences=clean_text(sentences)
+    query.extend(temp_sentences)
 
-    
+
+
 
 @app.route('/')
 def index():
@@ -191,11 +164,9 @@ def calculate_plagiarism():
     # Set input_string to the input text
     input_string = input_text
     query.clear()
-    sentences.clear()
-        
+    sentences.clear()     
     urls.clear()
     reference_documents.clear()
-
     score_tracker = 0
     score.clear()
     score.append(0)
@@ -203,13 +174,7 @@ def calculate_plagiarism():
     get_url()   
     # Calculate the average without the last element
     average_score = average_without_last_element(score) * 100
-    # print("QUERY: ",query)
-    # # print(sentences)
-    # print("SCORE: ",average_without_last_element(score))
-    # print("Plagarism is : ",average_without_last_element(score)*100,"%")
-    # print("SENTENCES: ",sentences)
     print(score)
-    print(query)
     return jsonify({'average_score': average_score})
 
 if __name__ == '__main__':
